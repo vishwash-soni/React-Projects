73968894a4209c6895db827b3df1e070c487d4ab
anubhav-soni <130144941+vishwash-soni@users.noreply.github.com>
2026-02-09 21:36:57 +0530

    nav complete

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-     vishwash soni
-    </>
-  )
+import "./App.css"
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
+const App = ()=>{
+  return <div>
+    <Navigation/>
+    <Hero/>
+  </div>
 }
 
-export default App
+
+export default App;
---
+++ project-1/src/components/Hero.jsx
@@ -0,0 +1,26 @@
+const Hero = () => {
+  return (
+    <main className="hero">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+        <div className="hero-btn">
+            <button>Shop Now</button>
+            <button>Category</button>
+        </div>
+        <div className="hero-shopping">
+            <p>Also Available On</p>
+            <div className="brand-icons">
+                <img src="./images/amazon.png" alt="amazon-logo" />
+                <img src="./images/flipkart.png" alt="flipkart-logo" />
+            </div>
+        </div>
+      </div>
+      <div className="hero-img">
+        <img src="./images/shoe_image.png" alt="hero-image" />
+      </div>
+    </main>
+  );
+};
+
+export default Hero;
